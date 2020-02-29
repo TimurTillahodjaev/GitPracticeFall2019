0673a5a4cf97642bac48838c26a0639fe84d6b15
Timur Tillahodjaev <ttillahodjaev@yahoo.com>
2020-02-29 15:26:47 -0500

    commit message

--- src/day01/String_Practice.java
+++ src/day01/String_Practice.java
@@ -4,6 +4,9 @@ public class String_Practice {
 
     public static void main(String[] args) {
         System.out.println("Hello word");
+        String str = "Java is  fun!";
+        System.out.println(str);
+
 
     }
 }
